201507bff75832fc034f47c868425d63296c6718
David Patzke <david.patzke@wheregroup.com>
2016-03-04 11:21:40 +0100

    Add BaseElement

---
+++ Element/BaseElement.php
@@ -0,0 +1,94 @@
+<?php
+namespace Mapbender\DataSourceBundle\Element;
+
+use Doctrine\DBAL\Connection;
+use Mapbender\CoreBundle\Element\HTMLElement;
+
+/**
+ * Class BaseElement
+ */
+class BaseElement extends HTMLElement
+{
+    /**
+     * Prepare elements recursive.
+     *
+     * @param $items
+     * @return array
+     */
+    public function prepareItems($items)
+    {
+        if (!is_array($items)) {
+            return $items;
+        } elseif (self::isAssoc($items)) {
+            $items = $this->prepareItem($items);
+        } else {
+            foreach ($items as $key => $item) {
+                $items[ $key ] = $this->prepareItem($item);
+            }
+        }
+        return $items;
+    }
+
+    /**
+     * Prepare element by type
+     *
+     * @param $item
+     * @return mixed
+     * @internal param $type
+     */
+    protected function prepareItem($item)
+    {
+        if (!isset($item["type"])) {
+            return $item;
+        }
+
+        if (isset($item["children"])) {
+            $item["children"] = $this->prepareItems($item["children"]);
+        }
+
+        switch ($item['type']) {
+            case 'select':
+                if (isset($item['sql'])) {
+                    $connectionName = isset($item['connection']) ? $item['connection'] : 'default';
+                    $sql            = $item['sql'];
+                    $options        = isset($item["options"]) ? $item["options"] : array();
+
+                    unset($item['sql']);
+                    unset($item['connection']);
+                    /** @var Connection $dbal */
+                    $dbal = $this->container->get("doctrine.dbal.{$connectionName}_connection");
+                    foreach ($dbal->fetchAll($sql) as $option) {
+                        $options[ current($option) ] = end($option);
+                    }
+                    $item["options"] = $options;
+                }
+
+                if (isset($item['service'])) {
+                    $serviceInfo = $item['service'];
+                    $serviceName = isset($serviceInfo['serviceName']) ? $serviceInfo['serviceName'] : 'default';
+                    $method      = isset($serviceInfo['method']) ? $serviceInfo['method'] : 'get';
+                    $args        = isset($serviceInfo['args']) ? $item['args'] : '';
+                    $service     = $this->container->get($serviceName);
+                    $options     = $service->$method($args);
+
+                    $item['options'] = $options;
+                }
+
+                if (isset($item['dataStore'])) {
+                    $dataStoreInfo = $item['dataStore'];
+                    $dataStore     = $this->container->get('data.source')->get($dataStoreInfo["id"]);
+                    $options       = array();
+                    foreach ($dataStore->search() as $dataItem) {
+                        $options[ $dataItem->getId() ] = $dataItem->getAttribute($dataStoreInfo["text"]);
+                    }
+                    if (isset($item['dataStore']['popupItems'])) {
+                        $item['dataStore']['popupItems'] = $this->prepareItems($item['dataStore']['popupItems']);
+                    }
+                    $item['options'] = $options;
+                }
+                break;
+        }
+        return $item;
+    }
+
+}
