6db7cbd63e35d04115717bbce5f62bdb53204564
Andriy Oblivantsev <eslider@gmail.com>
2016-09-28 11:15:19 +0200

    Add BaseController and DataStoreController for mapbender administration

---
+++ Controller/BaseController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Mapbender\DataSourceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Mapbender application management
+ */
+class BaseController extends Controller
+{
+    /**
+     * Get and optional decode JSON request data
+     *
+     * @return mixed
+     */
+    protected function getRequestData()
+    {
+        $content = $this->getRequest()->getContent();
+        $request = array_merge($_POST, $_GET);
+        if (!empty($content)) {
+            $request = array_merge($request, json_decode($content, true));
+        }
+        return $request;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getConfigurationPath()
+    {
+        $kernel     = $this->container->get("kernel");
+        $configPath = $kernel->getRootDir() . "/config";
+        return $configPath;
+    }
+}
---
+++ Controller/DataStoreController.php
@@ -0,0 +1,51 @@
+<?php
+namespace Mapbender\DataSourceBundle\Controller;
+
+use FOM\ManagerBundle\Configuration\Route;
+use FOM\ManagerBundle\Configuration\Route as ManagerRoute;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Mapbender application management
+ *
+ * @Route("datastore/")
+ */
+class DataStoreController extends BaseController
+{
+    /**
+     * Renders the layer service repository.
+     *
+     * @ManagerRoute("{page}", defaults={ "page"=1 }, requirements={ "page"="\d+" })
+     * @Method({ "GET" })
+     * @Template
+     */
+    public function indexAction($page)
+    {
+        return array(
+            'title' => 'DataStore',
+        );
+    }
+
+    /**
+     * Renders the layer service repository.
+     *
+     * @ManagerRoute("list")
+     */
+    public function listAction()
+    {
+        $request        = $this->getRequestData();
+        $kernel         = $this->container->get("kernel");
+        $configPath     = $kernel->getRootDir() . "/config";
+
+
+        return new JsonResponse(array(
+            'list' => array(),
+            'title'   => 'DataStore',
+            'request' => $request,
+            'path'    => $configPath
+        ));
+    }
+
+}
