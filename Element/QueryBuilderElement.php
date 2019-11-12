83cbca4b06e8b90820fb954fe7b303ab5a142dd3
werrolf <rolf.neuberger@wheregroup.com>
2019-11-12 18:29:44 +0100

    Remove Element stubs redundant with already required Mapbender >=3.0.8

--- Element/QueryBuilderElement.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Mapbender\DataSourceBundle\Element;
-
-/**
- * @deprecated use
- */
-class QueryBuilderElement extends \Mapbender\QueryBuilderBundle\Element\QueryBuilderElement
-{
-}
