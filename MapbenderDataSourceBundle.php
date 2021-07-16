f96f127d100d30103fddc9b240a156b0c906748e
werrolf <rolf.neuberger@wheregroup.com>
2021-07-16 15:06:04 +0200

    Replace non-extension with regular config load

--- MapbenderDataSourceBundle.php
+++ MapbenderDataSourceBundle.php
@@ -1,6 +1,10 @@
 <?php
 namespace Mapbender\DataSourceBundle;
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -10,4 +14,17 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class MapbenderDataSourceBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $configLocator = new FileLocator(__DIR__ . '/Resources/config');
+        $xmlLoader = new XmlFileLoader($container, $configLocator);
+        $xmlLoader->load('services.xml');
+        // Auto-rebuild on config change
+        $container->addResource(new FileResource($xmlLoader->getLocator()->locate('services.xml')));
+    }
+
+    public function getContainerExtension()
+    {
+        return null;
+    }
 }
